a07708791f6ca791e861ff4827a8a15d75f1be1a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-07 18:30:39 -0300

    c6- adding somethings modify about p

--- pages/index.js
+++ pages/index.js
@@ -28,12 +28,15 @@ const Home = () =>{
         
         <div className="mx-auto bg-red-100 text-center capitalize">
         <h1 className="font-bold"> Quem é o Felipe ?</h1>
-        <p className="italic">Eu sou desenvolvedor fullstack, tenho 27 anos e natural de Porto Alegre estado de Rio Grande do sul.</p>
+        <p className="italic">Eu Sou Desenvolvedor Fullstack, Tenho 27 Anos E Natural De Porto Alegre Estado Do Rio Grande Do Sul.
+
+        Sou Um Pessoa Bastante Focada E Ambiciosa Na Vida, Acredito Que Tenho Que Lutar Sempre Pelos Meus Objetivos De Vida.
         
-        <p className="italic"> Sou um pessoa bastante focada e ambiciosa na vida, acredito que tenho que lutar sempre pelos meus objetivos de vida.</p>
-        <p className="italic">Atualmente curso analise e desenvolvimento de sistemas, onde estou no 4 sementre. Busco ser um ótimo profissional e ótimo ser humano também.
-        Atualmente, me considero Intermediário na lingua inglesa e estou em busca da fluencia.</p>
-        <p className="italic">Meus objetivos no ramo são: Contribuir no mundo da tecnologia sendo um ótimo profissional e aprendendo com pessoas incriveis e claro fluente no ingles.</p>
+        Atualmente Curso Analise E Desenvolvimento De Sistemas, Onde Estou No 4 Sementre. Busco Ser Um Ótimo Profissional E Ótimo Ser Humano Também. Atualmente, Me Considero Intermediário Na Língua Inglesa E Estou Em Busca Da Fluência.
+        
+        Meus Objetivos No Ramo São: Contribuir No Mundo Da Tecnologia Sendo Um Ótimo Profissional E Aprendendo Com Pessoas Incríveis E Claro Fluente No Ingles.
+        
+        Minhas Experiências: Atualmente Minhas, Experiências São Realizadas No Desenvolvimento De Todos os Projetos Pessoais.</p>
         
         <h1>Minhas experiencias: Atualmente Minhas, experiencias são realizadas no desenvolvimento de todos projetos pessoais.</h1>
         </div>
