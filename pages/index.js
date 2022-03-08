8933df50f8e2fcfc9f1d1b39a2675b4fea9589f2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-07 21:22:24 -0300

    c18- adding new date

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,16 @@ const Home = () =>{
              <img id="icone" className="text-center inline" src="perfil.jpg"></img>
              <h1 className="font-bold text-center">Felipe Martins</h1>
              
-             <Link href='/sobre'>
-             <a className="font-bold">Sobre</a>
+             <Link href='/'>
+             <a className="italic">Sobre</a>
              </Link>
              
              <Link href='/about'>
-             <a className="font-bold m-2">About</a>
+             <a className="italic m-2">About</a>
              </Link>
 
              <Link href='/projetos'>
-             <a className="font-bold m-2">Projetos</a>
+             <a className="italic ">Projetos</a>
              </Link>
              
           </div>
@@ -27,8 +27,8 @@ const Home = () =>{
          
         
         <div className="mx-auto bg-yellow-300 text-center capitalize">
-        <h1 className="font-bold"> Quem é o Felipe?</h1>
-        <p className="italic">Eu Sou Desenvolvedor Fullstack, Tenho 27 Anos E Natural De Porto Alegre Estado Do Rio Grande Do Sul.
+        <h1 className="italic"> Quem é o Felipe?</h1>
+        <p className="italic"> Atualmente Sou Desenvolvedor Fullstack, Tenho 27 Anos E Natural De Porto Alegre Estado Do Rio Grande Do Sul.
 
         Sou Um Pessoa Bastante Focada E Ambiciosa Na Vida, Acredito Que Tenho Que Lutar Sempre Pelos Meus Objetivos De Vida.
         
@@ -48,7 +48,7 @@ const Home = () =>{
            <div id="fop" className="mx-auto text-center bg-white">
            <footer className="text-center mt-40">
            
-           <a href="https://github.com/felipe-programador-18?tab=overview&from=2022-03-01&to=2022-03-07"> <img id="hub" src="github.jpg"></img> </a>  
+           <a href="https://github.com/felipe-programador-18?tab=overview&from=2022-03-01&to=2022-03-07"> <img  id="hub" src="github.jpg"></img> </a>  
         <a href="https://www.linkedin.com/in/felipe-martins-programador/"> <img id="link" src="link.jpg"></img>  </a>       
            
            </footer>
